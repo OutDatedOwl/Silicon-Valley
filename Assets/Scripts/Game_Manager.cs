af2bcc07fd3b1db7d20acb42eab3f5541ab6e581
It's Ya Boi ScoobyDew <diegoaespitia@gmail.com>
2022-01-01 02:45:06 -0500

    new year update
    Jan 1 2022
    wow time flies

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -6,4 +6,16 @@ public class Game_Manager : MonoBehaviour
 {
     public AudioClip[] audioClipArchive;
 
+    [HideInInspector]
+    [SerializeField]
+    Camera[] scene_Camera_Array;
+
+    [HideInInspector]
+    [SerializeField]
+    Transform space_Ships;
+
+    private void Update(){
+        //scene_Camera_Array[0].transform.LookAt(space_Ships.position);
+    }
+
 }
--- Assets/Scripts/Intro_Script_Text.cs
+++ Assets/Scripts/Intro_Script_Text.cs
@@ -11,23 +11,42 @@ public class Intro_Script_Text : MonoBehaviour
     [SerializeField]
     private string[] dialogue_Text; // Array of stirngs used for dialogue
 
-    public string[] text_Parse_Array;
+    private string[] text_Parse_Array;
     
     [SerializeField]
     private Game_Manager game_Manager;
     
+    private Animator anim_One_Punch;
+    private Animator anim_Dog_Intro;
+    private Animator anim_Brain_Drain;
+
+    //[HideInInspector]
     [SerializeField]
     private AudioSource[] audio_Source_Array;
+    //audio_Source_0 = speech_Speed Sounds
+    //audio_Source_1 = wind_Moan_Ambience
+
+    [SerializeField]
+    private GameObject one_Punch_Man;
+    [SerializeField]
+    private GameObject brain_Drain;
+    [SerializeField]
+    private GameObject dog_Intro;
+
+    private char[] charSeparators = new char[] {'-',' '};
 
     private int next_Dialogue_Line;
 
-    public float speech_Speed;
+    [HideInInspector]
+    [SerializeField]
+    private float speech_Speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        //audio_Source_0 = speech_Speed Sounds
-        //audio_Source_1 = ambience
+        anim_One_Punch = one_Punch_Man.GetComponent<Animator>();
+        anim_Dog_Intro = dog_Intro.GetComponent<Animator>();
+        anim_Brain_Drain = brain_Drain.GetComponent<Animator>(); 
         audio_Source_Array[0] = game_Manager.GetComponent<AudioSource>();
         textMesh = GetComponent<TextMeshProUGUI>();
         //audio_Source.clip = game_Manager.audioClipArchive[0];
@@ -43,9 +62,11 @@ public class Intro_Script_Text : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.E)){
             Parse_Dialogue_Text();
             Dialogue_Audio(next_Dialogue_Line);
+            StopAllCoroutines();
             StartCoroutine(Dialogue_Beep_Boop());
             if(dialogue_Queue.Count != 0){
-                textMesh.text = dialogue_Queue.Dequeue();
+                //string sentence = dialogue_Queue.Dequeue();
+                //StartCoroutine(Type_Sentence(sentence));
             }
             //for(int i = 0; i < text_Parse_Array.Length; i++){
                 //StartCoroutine(Dialogue_Beep_Boop());
@@ -58,14 +79,39 @@ private void Dialogue_Audio(int dialogue_Line){
     if(dialogue_Line ==  5){
         audio_Source_Array[1].PlayOneShot(game_Manager.audioClipArchive[1]);
     }
-    if(dialogue_Line ==  10){
+    if(dialogue_Line == 6){
+        audio_Source_Array[2].PlayOneShot(game_Manager.audioClipArchive[3]);
+        one_Punch_Man.SetActive(true);
+    }
+    if(dialogue_Line ==  9){
+        audio_Source_Array[2].PlayOneShot(game_Manager.audioClipArchive[4]);
+        anim_One_Punch.SetBool("hologram_Finish", true);
+    }
+    if(dialogue_Line == 14){
         audio_Source_Array[1].PlayOneShot(game_Manager.audioClipArchive[2]);
+    }
+    if(dialogue_Line ==  15){
+        audio_Source_Array[2].PlayOneShot(game_Manager.audioClipArchive[3]);
+        dog_Intro.SetActive(true);
+        one_Punch_Man.SetActive(false);
+    }
+    if(dialogue_Line == 17){
+        audio_Source_Array[2].PlayOneShot(game_Manager.audioClipArchive[4]);
+        anim_Dog_Intro.SetBool("hologram_Finish", true);
+    }
+    if(dialogue_Line ==  18){
+        audio_Source_Array[2].PlayOneShot(game_Manager.audioClipArchive[5]);
+        brain_Drain.SetActive(true);
+        dog_Intro.SetActive(false);
+    }
+    if(dialogue_Line == 19){
+        anim_Brain_Drain.SetBool("brain_Drain_Finish", true);
     }
 }
 
     private void Parse_Dialogue_Text(){
         if(next_Dialogue_Line < dialogue_Text.Length){
-            text_Parse_Array = dialogue_Text[next_Dialogue_Line].Split(char.Parse(" "));
+            text_Parse_Array = dialogue_Text[next_Dialogue_Line].Split(charSeparators);
             next_Dialogue_Line++;
         }
         if(next_Dialogue_Line > text_Parse_Array.Length - 1){
@@ -74,10 +120,30 @@ private void Dialogue_Audio(int dialogue_Line){
     }
 
     private IEnumerator Dialogue_Beep_Boop(){
+        textMesh.text = "";
         for(int i = 0; i < text_Parse_Array.Length; i++){
+            textMesh.text += text_Parse_Array[i] + " ";
             yield return new WaitForSeconds(speech_Speed);
-            audio_Source_Array[0].pitch = Random.Range(0.8f, 1.0f);
-            audio_Source_Array[0].PlayOneShot(game_Manager.audioClipArchive[0]);
+            if(next_Dialogue_Line == 14){
+                break;
+            }
+            if(next_Dialogue_Line == 16){
+                break;
+            }
+            if(next_Dialogue_Line == 18){
+                break;
+            }
+            if(next_Dialogue_Line == 20){
+                break;
+            }
+            if(next_Dialogue_Line == 35){
+                break;
+            }
+            else
+            {
+                audio_Source_Array[0].pitch = Random.Range(0.8f, 1.0f);
+                audio_Source_Array[0].PlayOneShot(game_Manager.audioClipArchive[0]);
+            }
         }
     }
 }
